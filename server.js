987d74dad67fd50d802c0911d1b915c28ca5a20a
sunkireddy vasudeva reddy <svasudevareddy18604@gmail.com>
2025-05-09 20:45:38 +0530

    Your changes

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: 'https://delicute.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+app.use cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.static('public')); // Serve static files (index.html)
 
