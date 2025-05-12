c920dafbda6f22b5b932d813c7068eb9ecf29ef1
SUNKIREDDY VASUDEVA REDDY <svasudevareddy18604@gmail.com>
2025-05-12 19:36:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+    origin: [process.env.CLIENT_URL, 'https://delicute.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   },
